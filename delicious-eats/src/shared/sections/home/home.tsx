387f837f68c5edd0f0b429556b7a06f9eccd3326
zahairaJaada <zahaira.jaada@marketingconfort.com>
2025-09-27 14:43:02 +0200

    add restaurant atmosphere section

--- delicious-eats/src/shared/sections/home/home.tsx
+++ delicious-eats/src/shared/sections/home/home.tsx
@@ -1,10 +1,29 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 import Hero from "./components/hero";
+import RestaurantAtmosphere from "./components/restaurant-atmosphere";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const HomePage = () => {
+  useEffect(() => {
+    const initAOS = async () => {
+      await import("aos");
+      AOS.init({
+        duration: 1000,
+        easing: "ease",
+        once: false,
+        anchorPlacement: "top-bottom",
+      });
+    };
+    initAOS();
+  }, []);
+
   return (
     <div>
       <Hero />
+      <RestaurantAtmosphere />
     </div>
   );
 };
